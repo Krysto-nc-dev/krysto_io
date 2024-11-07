788903d33b28d4d61f9d8184d01d70ba576675e2
krysto-nc-dev <krysto.dev@gmail.com>
2024-11-07 16:06:53 +1100

    add mail template text

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,7 +57,7 @@ const registerUser = asyncHandler(async (req, res) => {
       await sendEmail({
         email: user.email,
         subject: 'Bienvenue sur Krysto.io!',
-         html :message,
+        message,
         isHtml: true, // Utilise le template HTML
       });
       console.log(`Email de bienvenue envoyé à ${user.email}`);
