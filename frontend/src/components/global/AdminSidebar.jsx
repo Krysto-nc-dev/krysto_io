4b5941ffd6b3e408edb1566321a1dad58b9ba9ab
krysto-nc-dev <krysto.dev@gmail.com>
2024-11-07 15:14:41 +1100

    logout user paht

--- frontend/src/components/global/AdminSidebar.jsx
+++ frontend/src/components/global/AdminSidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { logout } from '../path/to/your/slice/authSlice'; // Assurez-vous que le chemin d'import est correct
+import { logout } from '../../slices/authSlice'; // Assurez-vous que le chemin d'import est correct
 
 const AdminSidebar = ({ children }) => {
   const dispatch = useDispatch();
