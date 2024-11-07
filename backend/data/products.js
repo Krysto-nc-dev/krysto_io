26ac69f4c13a940c113cd0a38cecbb7a78fe4f03
Velten Stoyann <krysto.dev@gmail.com>
2024-11-07 19:35:57 +1100

    procduct sample image

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,56 +1,53 @@
 const products = [
-    {
-      name: 'Peignes grand modèle',
-      image: '/images/airpods.jpg',
-      description:
-        'Peignes de fabrication local en plastique recyclé',
-      price: 700,
-      countInStock: 10,
-      rating: 4.5,
-      numReviews: 12,
-    },
-    {
-      name: 'Peignes petit modèle',
-      image: '/images/airpods.jpg',
-      description:
-        'Peignes de fabrication local en plastique recyclé',
-      price: 700,
-      countInStock: 10,
-      rating: 4.5,
-      numReviews: 12,
-    },
-    {
-      name: 'Bagues',
-      image: '/images/airpods.jpg',
-      description:
-        'Bague de fabrication local en plastique recyclé',
-      price: 200,
-      countInStock: 10,
-      rating: 8.5,
-      numReviews: 12,
-    },
-    {
-      name: 'Perles boite de 100',
-      image: '/images/airpods.jpg',
-      description:
-        'Perles de fabrication local en plastique recyclé',
-      price: 1600,
-      countInStock: 10,
-      rating: 2.5,
-      numReviews: 12,
-    },
-    {
-      name: 'Perles boite de 1000',
-      image: '/images/airpods.jpg',
-      description:
-        'Perles de fabrication local en plastique recyclé',
-      price: 12000,
-      countInStock: 32,
-      rating: 1.5,
-      numReviews: 12,
-    },
+  {
+    name: "Peignes grand modèle",
+    image: "/images/sample.png",
 
-  ];
-  
-  export default products;
-  
+    description: "Peignes de fabrication local en plastique recyclé",
+    price: 700,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "Peignes petit modèle",
+    image: "/images/sample.png",
+    description: "Peignes de fabrication local en plastique recyclé",
+    price: 700,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "Bagues",
+    image: "/images/sample.png",
+
+    description: "Bague de fabrication local en plastique recyclé",
+    price: 200,
+    countInStock: 10,
+    rating: 8.5,
+    numReviews: 12,
+  },
+  {
+    name: "Perles boite de 100",
+    image: "/images/sample.png",
+
+    description: "Perles de fabrication local en plastique recyclé",
+    price: 1600,
+    countInStock: 10,
+    rating: 2.5,
+    numReviews: 12,
+  },
+  {
+    name: "Perles boite de 1000",
+    image: "/images/sample.png",
+
+    description: "Perles de fabrication local en plastique recyclé",
+    price: 12000,
+    countInStock: 32,
+    rating: 1.5,
+    numReviews: 12,
+  },
+];
+
+export default products;
